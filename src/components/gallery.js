b3235aceca25830c61bf9900a22df944214103ee
lachhab nacer <lachhab.nacer@gmail.com>
2022-12-19 03:41:07 +0100

    search bar in gallery component added and parameters for quering the api also

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -7,15 +7,25 @@ class Gallery extends Component {
         this.state={
             hits:[],
             currentPage:1,
-            pageSize:10,
-            currentkeyword:'rabat'
+            pageSize:12,
+            locationOfPictures:'',
+            totalPages:1
         };
     }
 
+    setLocationOfPictures=(event)=>{
+        this.setState({
+            locationOfPictures : event.target.value
+        });
+    };
+
     getHits(){
-        const city = 'paris';
+        const endPoint = 'https://pixabay.com/api/?key=';
+        const location = this.state.locationOfPictures;
         const key = '32134261-3095be03ae66ba0541849a4ba';
-        const url = 'https://pixabay.com/api/?key='+key+'&q='+city;
+        const currentPage = this.state.currentPage;
+        const sizePage = this.state.pageSize;
+        const url = endPoint+key+'&q='+location+'&page='+currentPage+'&per_page='+sizePage;
         axios.get(url)
              .then((resp)=>{
                  console.log(resp.data);
@@ -26,6 +36,11 @@ class Gallery extends Component {
              .catch((error)=>console.log(error));
     };//fin getHits
 
+    search=(event)=>{
+        event.preventDefault();
+        this.getHits();
+    };
+
     componentDidMount() {//cycle de vie du component, montage du component
         this.getHits();
     }
@@ -33,6 +48,20 @@ class Gallery extends Component {
     render() {
         return (
             <div>
+                <form onSubmit={this.search}>
+                    <div className="row m-2 p-2">
+                        <div className="col">
+                            <input type="text"
+                                   className="form-control text-center"
+                                   placeholder="search location"
+                                   value={this.state.locationOfPictures}
+                                   onChange={this.setLocationOfPictures}/>
+                        </div>
+                        <div className="col-auto">
+                            <button className="btn btn-success" type="submit">chercher</button>
+                        </div>
+                    </div>
+                </form>
                 <div className="row">
                     {this.state.hits.map(hit=>
                         <div className="col-md-4" key={hit.id}>
